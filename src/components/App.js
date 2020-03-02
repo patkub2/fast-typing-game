a476e35d94ddff4deb895ae635d49d2f0a476117
patkub2 <patrykkubala1999@gmail.com>
2020-03-02 19:06:41 +0100

    Working text change

--- src/components/App.js
+++ src/components/App.js
@@ -10,37 +10,42 @@ function App() {
   const [time, setTime] = useState(5);
   const [typed, setTyped] = useState("");
   const [correct, setCorrect] = useState("");
-  const [rest, setRest] = useState("");
+  const [rest, setRest] = useState(text);
 
   function Random() {
     var maxNumber = 100;
     var randomNumber = Math.floor(Math.random() * maxNumber + 1);
     return randomNumber;
   }
-
-  function handleChange(e) {
-    const { value } = e.target;
-    setTyped(value);
-  }
-
   String.prototype.removeCharAt = function(i) {
     var tmp = this.split(""); // convert to an array
     tmp.splice(i - 1, 1); // remove 1 element from the array (adjusting for non-zero-indexed counts)
     return tmp.join(""); // reconstruct the string
   };
 
+  function handleChange(e) {
+    const { value } = e.target;
+    //setTyped(value);
+    //console.log(value);
+    //console.log(text.charAt(0));
+    if (value == text.charAt(0)) {
+      setText(text.removeCharAt(1));
+      setCorrect(prevCorrect => prevCorrect + value);
+    }
+
+    console.log(correct);
+  }
+
   //console.log("crt/r2002_2".removeCharAt(4));
 
-  console.log(typed);
   return (
     <div className="App">
       <header className="App-header">
-        <a>{time}</a>
         <a>
-          {typed}
+          <span className="correct-char">{correct}</span>
           {text}
         </a>
-        <Timer />
+
         <textarea onChange={handleChange} value={typed} />
       </header>
     </div>
