bdcbd45434171108c0bf425a6535a3ef563e9560
Patryk Kubala <patrykkubala1999@gmail.com>
2020-06-12 18:21:56 +0200

    deleted useless end card

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import "../App.css";
 import Game from "./Game";
-import EndCard from "./EndCard";
 import WelcomePage from "./WelcomePage";
 import { Router, navigate } from "@reach/router";
 
@@ -17,7 +16,6 @@ function App() {
     <div>
       <Router>
         <Game path="game" gamerestart={gamerestart} />
-        <EndCard path="endcard" />
         <WelcomePage path="/" gamestart={gamestart} />
       </Router>
     </div>
